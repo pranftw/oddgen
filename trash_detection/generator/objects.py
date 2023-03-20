62b71b51b6f14bf5376e0590566d521ff36ed9ae
pranav <pranava.sri@gmail.com>
2023-03-20 11:54:50 +0530

    added annotations parsing from annotations.json and cropping. will add rest of them later with multiprocessing

--- trash_detection/generator/objects.py
+++ trash_detection/generator/objects.py
@@ -1,16 +1,24 @@
 from PIL import Image
-from utils import get_bbox
+from rembg import remove as rembg_remove
+from .utils import get_bbox
 
 
-def crop(img_paths, annotations):
+def crop(img_fpaths, annotations):
   '''
     annotations - (left, upper, width, height, category)
   '''
   cropped_objects = []
-  for img_path in self.img_paths:
-    with Image.open(img_path) as orig_img:
+  for img_fpath in self.img_fpaths:
+    with Image.open(img_fpath) as orig_img:
       for annotation in annotations:
         left, upper, width, height, category = annotation
         bbox = get_bbox(left, upper, width, height)
-        cropped_objects.append((orig_img.crop(bbox)), category)
+        cropped_objects.append((orig_img.crop(bbox), category))
   return cropped_objects
+
+
+def remove_bg(cropped_objects):
+  wo_bg_objects = []
+  for cropped_img,category in cropped_objects:
+    wo_bg_objects.append((rembg_remove(cropped_img), category))
+  return wo_bg_objects
--- trash_detection/generator/utils.py
+++ trash_detection/generator/utils.py
@@ -1,4 +1,14 @@
+import os
+import secrets
+
+
 def get_bbox(left, upper, width, height):
   right = left+width
   lower = upper+height
   return left, upper, width, height
+
+
+def save_objects(objects, fpath):
+  for object_img, category in objects:
+    object_fname = f'{category}_{secrets.token_hex(8)}.png'
+    object_img.save(os.path.join(fpath, object_fname))
