ca8ddb4b249cec5778f2a6fe783ff04f933f12d7
pranav <pranava.sri@gmail.com>
2023-03-29 16:04:19 +0530

    bg_remover model now in eval mode

--- trash_detection/generator/bg_remover.py
+++ trash_detection/generator/bg_remover.py
@@ -5,3 +5,4 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model = torch.jit.load('bg_remover_models/isnet_scripted_reduction_gpu.pt').to(device)
 model.load_state_dict(torch.load('bg_remover_models/isnet-general-use.pth'))
+model.eval()
