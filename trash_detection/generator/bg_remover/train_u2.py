d5556feac261b3d50faf49a7a5b9321cc1e8a9d8
pranav <pranava.sri@gmail.com>
2023-04-06 15:09:27 +0530

    removed a lot of trash from train_u2... haha. tqdm has to be added

--- trash_detection/generator/bg_remover/train_u2.py
+++ trash_detection/generator/bg_remover/train_u2.py
@@ -1,13 +1,15 @@
 '''
-	Taken from:
+	Referenced from:
 		https://github.com/xuebinqin/U-2-Net/blob/master/data_loader.py
 		https://github.com/xuebinqin/U-2-Net/blob/master/u2net_train.py
-	
+
+	NOTE: run this file directly, not as a part of the module
 	NOTE: scikit-image has to be installed using: pip install scikit-image
 '''
 
 
-from .remove import load_model
+import os
+from remove import load_model
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 from torch import nn
@@ -24,18 +26,14 @@ from torchvision import transforms, utils
 
 # <-----------------------------DATALOADER------------------------->
 
-#==========================dataset load==========================
 class RescaleT(object):
-
 	def __init__(self,output_size):
 		assert isinstance(output_size,(int,tuple))
 		self.output_size = output_size
 
 	def __call__(self,sample):
 		imidx, image, label = sample['imidx'], sample['image'],sample['label']
-
 		h, w = image.shape[:2]
-
 		if isinstance(self.output_size,int):
 			if h > w:
 				new_h, new_w = self.output_size*h/w,self.output_size
@@ -43,47 +41,13 @@ class RescaleT(object):
 				new_h, new_w = self.output_size,self.output_size*w/h
 		else:
 			new_h, new_w = self.output_size
-
 		new_h, new_w = int(new_h), int(new_w)
-
 		img = transform.resize(image,(self.output_size,self.output_size),mode='constant')
 		lbl = transform.resize(label,(self.output_size,self.output_size),mode='constant', order=0, preserve_range=True)
-
 		return {'imidx':imidx, 'image':img,'label':lbl}
 
-class Rescale(object):
-
-	def __init__(self,output_size):
-		assert isinstance(output_size,(int,tuple))
-		self.output_size = output_size
-
-	def __call__(self,sample):
-		imidx, image, label = sample['imidx'], sample['image'],sample['label']
-
-		if random.random() >= 0.5:
-			image = image[::-1]
-			label = label[::-1]
-
-		h, w = image.shape[:2]
-
-		if isinstance(self.output_size,int):
-			if h > w:
-				new_h, new_w = self.output_size*h/w,self.output_size
-			else:
-				new_h, new_w = self.output_size,self.output_size*w/h
-		else:
-			new_h, new_w = self.output_size
-
-		new_h, new_w = int(new_h), int(new_w)
-
-		# #resize the image to new_h x new_w and convert image from range [0,255] to [0,1]
-		img = transform.resize(image,(new_h,new_w),mode='constant')
-		lbl = transform.resize(label,(new_h,new_w),mode='constant', order=0, preserve_range=True)
-
-		return {'imidx':imidx, 'image':img,'label':lbl}
 
 class RandomCrop(object):
-
 	def __init__(self,output_size):
 		assert isinstance(output_size, (int, tuple))
 		if isinstance(output_size, int):
@@ -91,196 +55,84 @@ class RandomCrop(object):
 		else:
 			assert len(output_size) == 2
 			self.output_size = output_size
+
 	def __call__(self,sample):
 		imidx, image, label = sample['imidx'], sample['image'], sample['label']
-
 		if random.random() >= 0.5:
 			image = image[::-1]
 			label = label[::-1]
-
 		h, w = image.shape[:2]
 		new_h, new_w = self.output_size
-
 		top = np.random.randint(0, h - new_h)
 		left = np.random.randint(0, w - new_w)
-
 		image = image[top: top + new_h, left: left + new_w]
 		label = label[top: top + new_h, left: left + new_w]
-
 		return {'imidx':imidx,'image':image, 'label':label}
 
-class ToTensor(object):
-	"""Convert ndarrays in sample to Tensors."""
-
-	def __call__(self, sample):
-
-		imidx, image, label = sample['imidx'], sample['image'], sample['label']
-
-		tmpImg = np.zeros((image.shape[0],image.shape[1],3))
-		tmpLbl = np.zeros(label.shape)
-
-		image = image/255.
-		if(np.max(label)<1e-6):
-			label = label
-		else:
-			label = label/255.
-
-		if image.shape[2]==1:
-			tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
-			tmpImg[:,:,1] = (image[:,:,0]-0.485)/0.229
-			tmpImg[:,:,2] = (image[:,:,0]-0.485)/0.229
-		else:
-			tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
-			tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
-			tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
-
-		tmpLbl[:,:,0] = label[:,:,0]
-
-
-		tmpImg = tmpImg.transpose((2, 0, 1))
-		tmpLbl = label.transpose((2, 0, 1))
-
-		return {'imidx':torch.from_numpy(imidx), 'image': torch.from_numpy(tmpImg), 'label': torch.from_numpy(tmpLbl)}
 
 class ToTensorLab(object):
 	"""Convert ndarrays in sample to Tensors."""
-	def __init__(self,flag=0):
-		self.flag = flag
-
 	def __call__(self, sample):
-
-		imidx, image, label =sample['imidx'], sample['image'], sample['label']
-
+		imidx, image, label = sample['imidx'], sample['image'], sample['label']
 		tmpLbl = np.zeros(label.shape)
-
 		if(np.max(label)<1e-6):
 			label = label
 		else:
 			label = label/255.
-
-		# change the color space
-		if self.flag == 2: # with rgb and Lab colors
-			tmpImg = np.zeros((image.shape[0],image.shape[1],6))
-			tmpImgt = np.zeros((image.shape[0],image.shape[1],3))
-			if image.shape[2]==1:
-				tmpImgt[:,:,0] = image[:,:,0]
-				tmpImgt[:,:,1] = image[:,:,0]
-				tmpImgt[:,:,2] = image[:,:,0]
-			else:
-				tmpImgt = image
-			tmpImgtl = color.rgb2lab(tmpImgt)
-
-			# nomalize image to range [0,1]
-			tmpImg[:,:,0] = (tmpImgt[:,:,0]-np.min(tmpImgt[:,:,0]))/(np.max(tmpImgt[:,:,0])-np.min(tmpImgt[:,:,0]))
-			tmpImg[:,:,1] = (tmpImgt[:,:,1]-np.min(tmpImgt[:,:,1]))/(np.max(tmpImgt[:,:,1])-np.min(tmpImgt[:,:,1]))
-			tmpImg[:,:,2] = (tmpImgt[:,:,2]-np.min(tmpImgt[:,:,2]))/(np.max(tmpImgt[:,:,2])-np.min(tmpImgt[:,:,2]))
-			tmpImg[:,:,3] = (tmpImgtl[:,:,0]-np.min(tmpImgtl[:,:,0]))/(np.max(tmpImgtl[:,:,0])-np.min(tmpImgtl[:,:,0]))
-			tmpImg[:,:,4] = (tmpImgtl[:,:,1]-np.min(tmpImgtl[:,:,1]))/(np.max(tmpImgtl[:,:,1])-np.min(tmpImgtl[:,:,1]))
-			tmpImg[:,:,5] = (tmpImgtl[:,:,2]-np.min(tmpImgtl[:,:,2]))/(np.max(tmpImgtl[:,:,2])-np.min(tmpImgtl[:,:,2]))
-
-			# tmpImg = tmpImg/(np.max(tmpImg)-np.min(tmpImg))
-
-			tmpImg[:,:,0] = (tmpImg[:,:,0]-np.mean(tmpImg[:,:,0]))/np.std(tmpImg[:,:,0])
-			tmpImg[:,:,1] = (tmpImg[:,:,1]-np.mean(tmpImg[:,:,1]))/np.std(tmpImg[:,:,1])
-			tmpImg[:,:,2] = (tmpImg[:,:,2]-np.mean(tmpImg[:,:,2]))/np.std(tmpImg[:,:,2])
-			tmpImg[:,:,3] = (tmpImg[:,:,3]-np.mean(tmpImg[:,:,3]))/np.std(tmpImg[:,:,3])
-			tmpImg[:,:,4] = (tmpImg[:,:,4]-np.mean(tmpImg[:,:,4]))/np.std(tmpImg[:,:,4])
-			tmpImg[:,:,5] = (tmpImg[:,:,5]-np.mean(tmpImg[:,:,5]))/np.std(tmpImg[:,:,5])
-
-		elif self.flag == 1: #with Lab color
-			tmpImg = np.zeros((image.shape[0],image.shape[1],3))
-
-			if image.shape[2]==1:
-				tmpImg[:,:,0] = image[:,:,0]
-				tmpImg[:,:,1] = image[:,:,0]
-				tmpImg[:,:,2] = image[:,:,0]
-			else:
-				tmpImg = image
-
-			tmpImg = color.rgb2lab(tmpImg)
-
-			# tmpImg = tmpImg/(np.max(tmpImg)-np.min(tmpImg))
-
-			tmpImg[:,:,0] = (tmpImg[:,:,0]-np.min(tmpImg[:,:,0]))/(np.max(tmpImg[:,:,0])-np.min(tmpImg[:,:,0]))
-			tmpImg[:,:,1] = (tmpImg[:,:,1]-np.min(tmpImg[:,:,1]))/(np.max(tmpImg[:,:,1])-np.min(tmpImg[:,:,1]))
-			tmpImg[:,:,2] = (tmpImg[:,:,2]-np.min(tmpImg[:,:,2]))/(np.max(tmpImg[:,:,2])-np.min(tmpImg[:,:,2]))
-
-			tmpImg[:,:,0] = (tmpImg[:,:,0]-np.mean(tmpImg[:,:,0]))/np.std(tmpImg[:,:,0])
-			tmpImg[:,:,1] = (tmpImg[:,:,1]-np.mean(tmpImg[:,:,1]))/np.std(tmpImg[:,:,1])
-			tmpImg[:,:,2] = (tmpImg[:,:,2]-np.mean(tmpImg[:,:,2]))/np.std(tmpImg[:,:,2])
-
-		else: # with rgb color
-			tmpImg = np.zeros((image.shape[0],image.shape[1],3))
-			image = image/255.
-			if image.shape[2]==1:
-				tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
-				tmpImg[:,:,1] = (image[:,:,0]-0.485)/0.229
-				tmpImg[:,:,2] = (image[:,:,0]-0.485)/0.229
-			else:
-				tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
-				tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
-				tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
-
+		# change the color space flag=0
+		tmpImg = np.zeros((image.shape[0],image.shape[1],3))
+		image = image/255.
+		# normalizing
+		tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
+		tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
+		tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
 		tmpLbl[:,:,0] = label[:,:,0]
-
-
 		tmpImg = tmpImg.transpose((2, 0, 1))
 		tmpLbl = label.transpose((2, 0, 1))
-
 		return {'imidx':torch.from_numpy(imidx), 'image': torch.from_numpy(tmpImg), 'label': torch.from_numpy(tmpLbl)}
 
+
 class SalObjDataset(Dataset):
-	def __init__(self,img_name_list,lbl_name_list,transform=None):
-		self.image_name_list = img_name_list
-		self.label_name_list = lbl_name_list
+	def __init__(self, img_paths, lbl_paths, num_data, transform=None):
+		num_img_paths = len(img_paths)
+		num_lbl_paths = len(lbl_paths)
+		assert num_img_paths==num_lbl_paths, f'Total number of images and labels should match! Got {num_img_paths} images and {num_lbl_paths} labels.'
+		if num_data is not None:
+			assert num_data<=num_img_paths, f'number of data points required {num_data} should be lesser than or equal to total number of data {num_img_paths}'
+		self.num_data = num_data if num_data is not None else num_img_paths
+		self.img_paths, self.lbl_paths = zip(*(random.sample(zip(img_paths, lbl_paths), self.num_data)))
 		self.transform = transform
 
 	def __len__(self):
-		return len(self.image_name_list)
+		return len(self.img_paths)
 	
 	def __getitem__(self, idx):
-		image = np.asarray(Image.open(self.image_name_list[idx]).convert('RGB'))
+		image = np.asarray(Image.open(self.img_paths[idx]).convert('RGB')) # CxWxH
+		label = np.asarray(Image.open(self.lbl_paths[idx]))[:,:,np.newaxis] # WxH
 		imidx = np.array([idx])
-
-		if(0==len(self.label_name_list)):
-			label_3 = np.zeros(image.shape)
-		else:
-			label_3 = np.asarray(Image.open(self.label_name_list[idx]))
-
-		label = np.zeros(label_3.shape[0:2])
-		if(3==len(label_3.shape)):
-			label = label_3[:,:,0]
-		elif(2==len(label_3.shape)):
-			label = label_3
-
-		if(3==len(image.shape) and 2==len(label.shape)):
-			label = label[:,:,np.newaxis]
-		elif(2==len(image.shape) and 2==len(label.shape)):
-			image = image[:,:,np.newaxis]
-			label = label[:,:,np.newaxis]
-
 		sample = {'imidx':imidx, 'image':image, 'label':label}
-
 		if self.transform:
 			sample = self.transform(sample)
-
 		return sample
 
 
-# <-----------------------------TRAINER----------------------->
-
+# <-----------------------------TRAINING----------------------------->
 
 BATCH_SIZE = 8
+NUM_WORKERS = 1
+MODEL_SAVE_FREQ = 200
 EPOCHS = 100
 MODEL_PATH = 'models/u2netp_script_model.pt'
-SAVE_MODEL_WEIGHTS_IN = 'models/u2netp_weights/u2netp'
+SAVE_MODEL_WEIGHTS_IN = 'models/u2netp_weights/u2net_test'
+NUM_DATA = 100 # if None, use all the data in the dataset
 
-# ------- 1. define loss function --------
+try:
+	os.mkdir(SAVE_MODEL_WEIGHTS_IN)
+except FileExistsError:
+	print(f'Directory SAVE_MODEL_WEIGHTS_IN is not empty!')
 
 bce_loss = nn.BCELoss(reduction='mean')
-
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
-
 	loss0 = bce_loss(d0,labels_v)
 	loss1 = bce_loss(d1,labels_v)
 	loss2 = bce_loss(d2,labels_v)
@@ -288,87 +140,59 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 	loss4 = bce_loss(d4,labels_v)
 	loss5 = bce_loss(d5,labels_v)
 	loss6 = bce_loss(d6,labels_v)
-
 	loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
 	# print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),loss4.data.item(),loss5.data.item(),loss6.data.item()))
-
 	return loss0, loss
 
 
-# ------- 2. set the directory of training dataset --------
 with open('trash_detection/ignore/crop_masks/images.txt') as fp:
-	tra_img_name_list = fp.read().split('\n')
+	img_paths = fp.read().split('\n')
 with open('trash_detection/ignore/crop_masks/labels.txt') as fp:
-	tra_lbl_name_list = fp.read().split('\n')
-
-train_num = len(tra_img_name_list)
-
+	lbl_paths = fp.read().split('\n')
+train_num = len(img_paths)
 salobj_dataset = SalObjDataset(
-		img_name_list=tra_img_name_list,
-		lbl_name_list=tra_lbl_name_list,
+		img_paths=img_paths,
+		lbl_paths=lbl_paths,
+		num_data=NUM_DATA,
 		transform=transforms.Compose([
-				RescaleT(320),
-				RandomCrop(288),
-				ToTensorLab(flag=0)]))
-salobj_dataloader = DataLoader(salobj_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=1)
+			RescaleT(320),
+			RandomCrop(288),
+			ToTensorLab()
+		]))
+salobj_dataloader = DataLoader(salobj_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 
-# ------- 3. define model --------
 model, device = load_model(MODEL_PATH, None, strict_weights_loading=False)
-model.train()
+optim = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
 
-# ------- 4. define optimizer --------
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
-
-# ------- 5. training process --------
-print("---start training...")
 ite_num = 0
 running_loss = 0.0
 running_tar_loss = 0.0
 ite_num4val = 0
-save_frq = 2000 # save the model every 2000 iterations
-
 for epoch in range(0, EPOCHS):
 	model.train()
-
 	for i, data in enumerate(salobj_dataloader):
 		ite_num = ite_num + 1
 		ite_num4val = ite_num4val + 1
 
-		inputs, labels = data['image'], data['label']
+		inputs, labels = data['image'].type(torch.FloatTensor), data['label'].type(torch.FloatTensor)
+		inputs_v, labels_v = Variable(inputs.to(device), requires_grad=False), Variable(labels.to(device), requires_grad=False)
 
-		inputs = inputs.type(torch.FloatTensor)
-		labels = labels.type(torch.FloatTensor)
-
-		# wrap them in Variable
-		if torch.cuda.is_available():
-			inputs_v, labels_v = Variable(inputs.cuda(), requires_grad=False), Variable(labels.cuda(),
-																																										requires_grad=False)
-		else:
-			inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(labels, requires_grad=False)
-
-		# y zero the parameter gradients
-		optimizer.zero_grad()
-
-		# forward + backward + optimize
+		optim.zero_grad()
 		d0, d1, d2, d3, d4, d5, d6 = model(inputs_v)
 		loss2, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
-
 		loss.backward()
-		optimizer.step()
+		optim.step()
 
-		# # print statistics
 		running_loss += loss.data.item()
 		running_tar_loss += loss2.data.item()
-
-		# del temporary outputs and loss
 		del d0, d1, d2, d3, d4, d5, d6, loss2, loss
 
 		print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
 		epoch + 1, EPOCHS, (i + 1) * BATCH_SIZE, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
 
-		if ite_num % save_frq == 0:
-			torch.save(model.state_dict(), SAVE_MODEL_WEIGHTS_IN+"_bce_itr_%d_train_%3f_tar_%3f.pth" % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+		if ite_num%MODEL_SAVE_FREQ==0:
+			torch.save(model.state_dict(), SAVE_MODEL_WEIGHTS_IN+"/bce_itr_%d_train_%3f_tar_%3f.pth" % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
 			running_loss = 0.0
 			running_tar_loss = 0.0
-			model.train()  # resume train
+			model.train()
 			ite_num4val = 0
